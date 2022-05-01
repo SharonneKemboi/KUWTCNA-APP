cafe14473f9b05bc72301879210d76123d69f656
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-02 02:05:40 +0300

    Add a navbar template file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,14 @@
 from turtle import title
 from flask import render_template
+from app.model import Articles
+
+from app.request import get_articles
 from .import main
 
 @main.route('/')
 def index():
 
+    articles = get_articles()
+
     title = 'KUWTCNA Articles'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, news= articles)
