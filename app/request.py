de6e83b3364c65f65b623c67eb0f476cba775f4a
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-02 01:54:23 +0300

    create a function that will process the results

--- app/request.py
+++ app/request.py
@@ -33,4 +33,23 @@ def get_articles():
             
 
     return articles_results
+def process_results(articles_list):
+    
+    articles_results = []
+
+    for articles_item in articles_list:
+        title = articles_item.get('title')
+        description = articles_item.get('description')
+        imageurl = articles_item.get('imageurl')
+        content = articles_item.get('content')
+        date = articles_item.get('date')
+        url = articles_item.get('url')
+
+        articles_object = Articles(title, description, imageurl, content, date, url)
+       
+        articles_results.append(articles_object)
+        
+    return articles_results
+
+    
 
