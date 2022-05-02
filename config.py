52fd5997f73b9d0d1f0c8e6d9631b5d0b4aa4ca1
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-02 04:37:44 +0300

    Add APIKey and Base Url

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e1748305ab8746c89ef63618e01e7b8e'
+    NEWS_API_KEY = os.environ.get('e1748305ab8746c89ef63618e01e7b8e')
 
 
 
@@ -25,3 +28,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig
+}
