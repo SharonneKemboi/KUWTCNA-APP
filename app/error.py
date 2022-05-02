4174c8d959edb63e081157b7562fbc0db91be413
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-02 04:44:35 +0300

    define error handler

---
+++ app/error.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app
+from urllib import error
+
+@app.errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
+
+
+@app.errorhandler(error.HTTPError)
+def http_error(error):
+    return render_template('404_page.html'), 404
