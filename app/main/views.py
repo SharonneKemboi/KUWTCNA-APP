022bcd43911211d383576bb312073611c78e38ba
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-02 06:47:31 +0300

    Add a search function

--- app/main/views.py
+++ app/main/views.py
@@ -11,12 +11,8 @@ def index():
 
     title = 'KUWTCNA Articles'
 
-    search_news = request.args.get('news_query')
-
-    if search_news:
-        return redirect(url_for('search',article_name=search_news))
-    else:
-        return render_template('index.html', title = title, news= articles)
+    
+    return render_template('index.html', title = title, news= articles)
     
 
 @main.route('/search/<article_name>')
@@ -24,8 +20,13 @@ def search(article_name):
     '''
     View function to display the search results
     '''
-    article_name_list = article_name.split(" ")
-    article_name_format = "+".join(article_name_list)
-    searched_article = search_news(article_name_format)
-    title = f'search results for {article_name}'
-    return render_template('search.html',article = searched_article)    
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search',article_name=search_news))
+    else:
+     article_name_list = article_name.split(" ")
+     article_name_format = "+".join(article_name_list)
+     searched_article = search_news(article_name_format)
+     title = f'search results for {article_name}'
+     return render_template('search.html',article = searched_article)    
